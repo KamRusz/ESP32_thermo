c3973ca38b454c2a84d93692e5f9038273f6d421
Kamil <kamilrusztyn@gmail.com>
2022-03-18 18:22:36 +0100

    remove sql - have to fix localy first

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app= Flask(__name__)
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 from app import routes

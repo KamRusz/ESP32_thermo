4c0cc53a6c340908c79f504b47e0ab893749516e
Kamil <kamilrusztyn@gmail.com>
2022-03-18 17:56:28 +0100

    new routes added

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,22 @@ def index():
     return render_template('index.html', title='Home')
 
 slownik = {"target_temp":"25", "user_override":"1"}
+
+@app.route('/hello', methods=['GET','POST'])
+def welcome():   
+    payload = json.loads(request.data)
+    print("api =",payload["api_key"])
+    print("room_temp =",payload["room_temp"])
+    print("room_humi =",payload["room_humi"])
+    return slownik
+    #print(request.args["temp"],request.args["humi"])
+    #if api_key == "123456789":
+    #    return slownik
+    #else:
+    #    return "zonk"
+
+@app.route('/hello2', methods=['GET','POST'])
+def welcome2():   
+    payload = json.loads(request.data)
+    print("user_temp =",payload["user_temp"])
+    return "user temp ovveride"

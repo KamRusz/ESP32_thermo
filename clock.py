2a9eff392b3557791ed768cb9c70429a255a1bf0
Kamil <kamilrusztyn@gmail.com>
2022-03-28 18:53:00 +0200

    dates testing file

--- clock.py
+++ clock.py
@@ -1,9 +1,42 @@
+from datetime import datetime, date
 from apscheduler.schedulers.blocking import BlockingScheduler
+from app.main import app, db
+from app.models import Temphumi, Avg_temphumi
 
 sched = BlockingScheduler()
 
+def Average(lst):
+    try: 
+        x = round(sum(lst) / len(lst),2)
+    except ZeroDivisionError:
+        return 0
+    return x
+
+x=str(datetime.now())
+        
+
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
+    print("datetime.now()[:10]", x[:10])
+    print("date.today()",date.today())
+    print('datetime.today().strftime("%Y-%m-%d")',datetime.today().strftime("%Y-%m-%d"))
+
+'''
+@sched.scheduled_job('cron', hour=23)
+def timed_job():
+    data = db.session.query(Temphumi).filter(Temphumi.day==datetime.today().strftime("%Y-%m-%d")).#grupowanie średnia all()
+    avgt=[]
+    avgh=[]
+    for x in data:
+        avgt.append(x.temp)
+        avgh.append(x.humi)
+    u = Avg_temphumi(avg_temp = Average(avgt), avg_humi=Average(avgh))
+    if db.session.query(Avg_temphumi).filter(Avg_temphumi.day==datetime.today().strftime("%Y-%m-%d")).all():
+        pass
+    else:
+        db.session.add(u)
+        db.session.commit()
+'''
+
 
 sched.start()

ddf0738132fdd843f6d1e7e4d5b46c21b35ff27f
Kamil <kamilrusztyn@gmail.com>
2022-03-29 17:18:58 +0200

    testing queries with clock

--- clock.py
+++ clock.py
@@ -17,7 +17,7 @@ def Average(lst):
     return x
 
 
-@sched.scheduled_job("cron", hour=23)
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
     # data = db.session.query(db.func.avg(Temphumi)).filter(Temphumi.day==date.today()).first()
     data = db.session.query(Temphumi).filter(Temphumi.day == datetime.now().strftime("%Y-%m-%d")).all()
@@ -28,10 +28,12 @@ def timed_job():
         avgh.append(x.humi)
     u = Avg_temphumi(avg_temp=Average(avgt), avg_humi=Average(avgh))
     if db.session.query(Avg_temphumi).filter(Avg_temphumi.day == datetime.now().strftime("%Y-%m-%d")).all():
+        print(datetime.now().strftime("%Y-%m-%d"), "row already exist")
         pass
     else:
         db.session.add(u)
         db.session.commit()
+        print(datetime.now().strftime("%Y-%m-%d"), "row added")
     #deleting old temp data - older then X days
     expendable = datetime.now() - timedelta(days = Config.KEEP_LOG_DAYS)
     db.session.query(Temphumi).filter(Temphumi.day < expendable.strftime("%Y-%m-%d")).delete()
